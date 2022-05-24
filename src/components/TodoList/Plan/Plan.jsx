913deca9ad6670524acdfd9f6f74bd6db35593e0
KROKODILMALBORO <krokodilmalboro@gmail.com>
2022-05-25 02:20:30 +0700

    Code correction in Plan.jsx

--- src/components/TodoList/Plan/Plan.jsx
+++ src/components/TodoList/Plan/Plan.jsx
@@ -3,24 +3,21 @@ import React from 'react'
 const Plan = (props) => {
     return (
         <div className='d-flex flex-row justify-content-center mb-3'>
-            <div className='d-flex justify-content-between list-group-item list-group-item-action rounded shadow-sm w-50'>
+            <div
+                className='d-flex justify-content-between list-group-item list-group-item-action rounded shadow-sm w-50'
+                onClick={props.pathListPlan}
+                role='button'
+            >
                 <span className='fs-4'>
                     {props.plan.name}
                 </span>
-                <div onClick={props.pathListPlan}>
-                    {
-                        props.plan.isCompleted ?
-                        <input
-                            className='form-check-input p-3 mb-1'
-                            type='checkbox'
-                            checked
-                        /> :
-                        <input
-                            className='form-check-input p-3 mb-1'
-                            type='checkbox'
-                        />
-                    }
-                </div>
+                <input
+                    className='form-check-input p-3 mb-1'
+                    type='checkbox'
+                    checked={props.plan.isCompleted}
+                    onChange={props.pathListPlan}
+                    onClick={props.pathListPlan}
+                />
             </div>
         </div>
     )
